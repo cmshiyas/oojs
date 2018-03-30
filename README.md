8cad19c8f3aa49513d94ec6091440813174f10e1
Shiyas Cholamukhath <cmshiyas007@gmail.com>
2018-03-31 01:00:21 +1100

    Created object from an existing Object

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This time we made the Objects little complex in nature.
+Here we created an Object from an Existing Object using create method.
+
+The new Object "person2" has the same properties and method available to person1.
--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function Person(first, last, age, gender, interests) {
         return (`${this.name.first} ${this.name.last} is ${this.age} years old. ${this.salutation} likes ${this.interests[0]} and ${this.interests[1]} .`);
       };
     this.greeting = function() {
-        alert('Hi! I\'m ' + this.name.first + '.');
+        return ('Hi! I\'m ' + this.name.first + '.');
       };
 }
 
@@ -21,5 +21,13 @@ var htmlElement = document.getElementById("root");
 var element = document.createElement("p");  
 var bio = document.createTextNode(`${person1.bio()}`);
 element.appendChild(bio);
+
+htmlElement.appendChild(element);
+
+var person2 = Object.create(person1);
+
+var element = document.createElement("p");  
+var greeting = document.createTextNode(`${person2.greeting()}`);
+element.appendChild(greeting);
 
 htmlElement.appendChild(element);
